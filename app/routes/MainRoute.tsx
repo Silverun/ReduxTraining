fc6d72663448a57f9c59c5357230bd474b655599
silverun <imonutiy@gmail.com>
2024-09-05 14:51:13 +0300

    add badge cart

--- app/routes/MainRoute.tsx
+++ app/routes/MainRoute.tsx
@@ -4,15 +4,22 @@ import HomeScreen from '../screens/home/HomeScreen';
 import ProfileScreen from '../screens/profile/ProfileScreen';
 import {Routes} from '.';
 import CartScreen from '../screens/cart/CartScreen';
+import {useAppSelector} from '../hooks/Redux';
 
 const BotTab = createBottomTabNavigator();
 
 const MainRoute = () => {
+  const cartItemsCount = useAppSelector(state => state.cart.items.length);
+
   return (
     <BotTab.Navigator screenOptions={{headerShown: false}}>
       <BotTab.Screen name={Routes.Home} component={HomeScreen} />
       <BotTab.Screen name={Routes.Profile} component={ProfileScreen} />
-      <BotTab.Screen name={Routes.Cart} component={CartScreen} />
+      <BotTab.Screen
+        name={Routes.Cart}
+        component={CartScreen}
+        options={{tabBarBadge: cartItemsCount ? cartItemsCount : undefined}}
+      />
     </BotTab.Navigator>
   );
 };
