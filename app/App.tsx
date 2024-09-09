d34af1deade02bdd45cd34f758fc3445fc668132
Silverun <imonutiy@gmail.com>
2024-09-09 13:03:38 +0300

    EOL

--- app/App.tsx
+++ app/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import {Provider} from 'react-redux';
 import Main from './Main';
 import {NavigationContainer} from '@react-navigation/native';
-import store from './store';
 
 function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Main />
-      </NavigationContainer>
-    </Provider>
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
   );
 }
-
+s;
 export default App;
