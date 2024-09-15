f0ba21c3640e7bce7a21ca4b7af56bf83a86abe0
silverun <imonutiy@gmail.com>
2024-09-15 21:59:24 +0300

    updated babel

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ import {AppRegistry} from 'react-native';
 import App from './app/App';
 import {name as appName} from './app.json';
 
+if (
+  !new (class {
+    x;
+  })().hasOwnProperty('x')
+)
+  throw new Error('Transpiler is not configured correctly');
+
 AppRegistry.registerComponent(appName, () => App);
